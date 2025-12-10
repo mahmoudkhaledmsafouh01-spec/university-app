7f483ce0aeba41f2f8188d55a0156d35c9779c01
Mahmoud Khaled <mahmoud@example.com>
2025-12-10 03:18:00 +0200

    updated changes

--- lib/auth.ts
+++ lib/auth.ts
@@ -40,7 +40,7 @@ export const authOptions: AuthOptions = {
       },
     },
   },
-  
+
   providers: [
     CredentialsProvider({
       name: "credentials",
@@ -111,5 +111,4 @@ export const authOptions: AuthOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
